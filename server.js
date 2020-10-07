d2ca59cec8bdc6ef72737491a26bfe884ea24516
diya-pkuddupadav <praveen.ku@endurance.com>
2020-10-08 02:17:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,57 +1,60 @@
-  require('dotenv').config({ path: 'variables.env' });
+require('dotenv').config({ path: 'variables.env' });
 
-  const express = require('express')
-  const path = require('path')
-  const bodyParser = require('body-parser')
-  const app = express()
-  const Pusher = require('pusher')
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const app = express()
+const Pusher = require('pusher')
+const HTTP_PORT = process.env.PORT || 5000;
 
-  const pusher = new Pusher({
-    appId: process.env.PUSHER_APP_ID,
-    key: process.env.PUSHER_APP_KEY,
-    secret: process.env.PUSHER_APP_SECRET,
-    cluster: process.env.PUSHER_APP_CLUSTER,
-    encrypted: true,
-  })
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_APP_KEY,
+  secret: process.env.PUSHER_APP_SECRET,
+  cluster: process.env.PUSHER_APP_CLUSTER,
+  encrypted: true,
+})
 
-  app.use(bodyParser.json())
-  app.use(bodyParser.urlencoded({ extended: false }))
-  app.use(express.static(path.join(__dirname)));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname)));
 
-  app.use((req, res, next) => {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    // Pass to next layer of middleware
-    next()
-  })
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  // Pass to next layer of middleware
+  next()
+})
 
-  app.set('PORT', process.env.PORT || 5000);
+//app.set('PORT', process.env.PORT || 5000);
 
-  app.get('/', (req, res) => {
-      res.sendFile(path.join(__dirname + '/index.html'));
-  });
+app.post('/Retrospective/New', (req, res) => {
+  const payload = req.body;
+  pusher.trigger('retro-data', 'content', payload);
+    res.sendStatus(200);
+});
 
-  app.post('/Retrospective/New', (req, res) => {
-    const payload = req.body;
-    pusher.trigger('retro-data', 'content', payload);
-      res.sendStatus(200);
-  });
-
-if (process.env.NODE_ENV === "production") {
-  // Set the static assets folder (ie, client build)
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
   app.use(express.static('client/build'));
-  app.get('*', (req,res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', index.html'))
+
+  // Express serve up index.html file if it doesn't recognize route
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
-  app.listen(app.get('PORT'), () =>{
-    console.log('Listening at ' + app.get('PORT'))
-  })
+app.listen(HTTP_PORT, err => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('Listening at ' + HTTP_PORT);
+  }
+})
